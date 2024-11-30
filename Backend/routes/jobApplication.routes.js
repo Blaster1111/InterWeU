ccacb3e76b03ddba5df43f75d95176c4d69a5434
Blaster1111 <rudrashukla0711@gmail.com>
2024-11-30 22:47:45 +0530

    job application modified

--- Backend/routes/jobApplication.routes.js
+++ Backend/routes/jobApplication.routes.js
@@ -1,17 +1,17 @@
-import express from "express";
-import multer from "multer";
-import { createJobApplication, updateJobApplicationStatus,getCandidateApplications,getJobApplicationsEmployee } from "../controllers/jobApplication.controllers.js";
+    import express from "express";
+    import multer from "multer";
+    import { createJobApplication, updateJobApplicationStatus,getCandidateApplications,getJobApplicationsEmployee } from "../controllers/jobApplication.controllers.js";
 
-const upload = multer({ dest: 'uploads/' });
+    const upload = multer({ dest: 'uploads/' });
 
-const router = express.Router();
+    const router = express.Router();
 
-router.post('/apply', upload.single('Resume'), createJobApplication);
+    router.post('/apply', upload.single('Resume'), createJobApplication);
 
-router.put('/apply/:id', updateJobApplicationStatus);
+    router.put('/apply/:id', updateJobApplicationStatus);
 
-router.get('/apply',getCandidateApplications);
+    router.get('/apply',getCandidateApplications);
 
-router.get("/:jobId/applications", getJobApplicationsEmployee);
+    router.get("/:jobId/applications", getJobApplicationsEmployee);
 
-export default router;
+    export default router;
